12f1636e2f171f74db7b09fec7ff103f7fc84634
mj342 <mj342@njit.edu>
2019-11-28 01:13:20 +0000

    Updating

--- scripts/APItest.php
+++ scripts/APItest.php
@@ -11,33 +11,81 @@ function search($source){
 	// Set some options, we are passing in a useragent too 
 	curl_setopt_array($curl, [
 		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL =>"http://open.api.ebay.com/shopping?callname=FindProducts&responsencoding=JSON&siteid=0&version=967&QueryKeywords=$source&AvailableItemsOnly=true&MaxEntries=5&appid=$apikey",
+		CURLOPT_URL =>"http://open.api.ebay.com/shopping?callname=FindProducts&responsencoding=JSON&siteid=0&version=967&QueryKeywords=$source&AvailableItemsOnly=true&MaxEntries=41&appid=$apikey",
 		CURLOPT_USERAGENT => 'API sample cURL Request for sneakers',
 	]);
 	//send the request & save the response to $resp
 	$resp = curl_exec($curl);
-	//echo the response
-	echo $resp;
+
 	//close request to clear up some resources
 	curl_close($curl);
-	//NEW CODE
-	foreach($resp -> searchResult -> item -> as $item)
-		{
-			$apiArray[$input] = array(
-			"pic" => $item -> galleryURL;
-			"link" => $item -> viewItemURL;
-			"title" => $item -> title;
-			"price" => $item -> sellingStatus -> currentPrice;
-			);
-		}
-
-	}
-	//return $resp;
-	return $apiArray;
 	
+	return $resp;
 
-//---------------------------------------------------------------------------------------------------------------------------------
+/*
+	$devices = array();
+
+        $apiXML = simplexml_load_string($resp);	
+	foreach ($apiXML -> Product as $item)
+	{
+        	$device = array();
+        
+		foreach ($item as $key => $value)
+		{
+			$device[(string) $key] = (string) $value;
+        	}
+		$devices[] = $device;
+	}
+	print_r($devices);
+	$api_dataa = "";
+	foreach ($devices as $item) 
+	{	
+		$price = "Price: $" . rand(40,120);
+
+		if ($item['DisplayStockPhotos'] == "true")
+		{
+			$api_dataa .=  $item ['ProductID'] . "<br>";
+			$api_dataa .= $item['DisplayStockPhotos']
+				      . "<br>";		
+			$api_dataa .=  $item ['Title'] . "<br>";
+			$api_dataa .= $price . "<br>";
+			$api_dataa .= "<img src = " . $item ['StockPhotoURL'] . 
+					">" . "<br><br>";
+			
+		}
+	}
+	return $api_dataa;
+*/
 }
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
