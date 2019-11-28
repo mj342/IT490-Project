22da1354b837e1b9fc659cb9d34576d3a79c6c7d
Suliman9 <amrsoliman0@gmail.com>
2019-11-28 01:22:01 +0000

    updating apitest.php

--- scripts/APItest.php
+++ scripts/APItest.php
@@ -6,13 +6,13 @@ function search($source)
 	$curl = curl_init();
 	curl_setopt_array($curl, [
 		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL =>"http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&siteid=0&version=967&QueryKeywords=$source&AvailableItemsOnly=true&MaxEntries=20&appid=$apikey",
+		CURLOPT_URL =>"http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&siteid=0&version=967&QueryKeywords=$source&AvailableItemsOnly=true&MaxEntries=41&appid=$apikey",
 		CURLOPT_USERAGENT => 'API sample cURL Request for sneakers',
 	]);
 	//send the request & save the response to $resp
 	$resp = curl_exec($curl);
 	//echo the response
-	echo $resp;
+	//echo $resp;
 	//close request to clear up some resources
 	curl_close($curl);
 	return $resp;
